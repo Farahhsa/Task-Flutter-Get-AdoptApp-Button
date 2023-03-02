4066e9771888dc970cc0dec5565b71ba593d40a1
Farah <farahaladwanii24@gmail.com>
2023-03-02 18:11:09 +0300

    first commit

--- lib/providers/pets_provider.dart
+++ lib/providers/pets_provider.dart
@@ -1,7 +1,14 @@
 import 'package:adopt_app/models/pet.dart';
 import 'package:flutter/material.dart';
 
+import '../services/pet_services.dart';
+
 class PetsProvider extends ChangeNotifier {
+  Future getpets() async {
+    pets = await PetServices().getPets();
+    notifyListeners();
+  }
+
   List<Pet> pets = [
     Pet(
         name: "Lucifurr",
