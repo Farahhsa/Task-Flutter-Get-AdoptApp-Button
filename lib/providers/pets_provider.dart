b86cb0da68faa1f9ac07ec1b411e768d90abbb58
Farah <farahaladwanii24@gmail.com>
2023-03-06 10:52:01 +0300

    done

--- lib/providers/pets_provider.dart
+++ lib/providers/pets_provider.dart
@@ -1,19 +1,32 @@
 import 'package:adopt_app/models/pet.dart';
+import 'package:adopt_app/services/pets_services.dart';
 import 'package:flutter/material.dart';
 
-import '../services/pet_services.dart';
-
 class PetsProvider extends ChangeNotifier {
-  Future getpets() async {
-    pets = await PetServices().getPets();
+  List<Pet> pets = [];
+  // Actions
+  Future getPets() async {
+    final petsResponse = await DioClient.shared.getPets();
+    pets = petsResponse;
     notifyListeners();
   }
 
-  List<Pet> pets = [
-    Pet(
-        name: "Lucifurr",
-        image: "https://i.ibb.co/P6VJ4pZ/smile-cat-1.png",
-        age: 2,
-        gender: "male")
-  ];
+  Future addPet(Pet newpet) async {
+    await DioClient.shared.addNewPet(newpet);
+    // pets = petsData;
+    notifyListeners();
+  }
+
+  Future editPet(Pet editedPet) async {
+    await DioClient.shared.editNewPet(editedPet);
+    getPets();
+    // pets = petsData;
+    notifyListeners();
+  }
+
+  void deletePet(int petId) async {
+    await DioClient.shared.deletePet(petId);
+    pets.removeWhere((pet) => pet.id == petId);
+    notifyListeners();
+  }
 }
